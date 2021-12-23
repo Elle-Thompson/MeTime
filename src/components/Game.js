cc75b7bf37c53ddbe4c95684693102900278f0e4
Elle Thompson <elletee@Elles-iMac.attlocal.net>
2021-12-22 17:21:20 -0800

    added additonal buttons for increased difficulty

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,7 +3,9 @@ import { useState} from "react";
 
 const Game = () => {
   const choiceList = ["Turn it!", "Click it!", "Slide it!"];
-  const buttonsList = ["Turn", "Click", "Slide"]
+  // let buttonsList = ["Turn", "Click", "Slide"]
+  // let shuffledArray = []
+  const [buttonsList, setButtonsList] = useState(["Turn", "O_O ", "Click", "O_O ", "Slide", "O_O "]);
   const [currentChoice, setCurrentChoice] = useState();
   // const [button1, setButton1] = useState("");
   // const [button2, setButton2] = useState("");
@@ -11,40 +13,21 @@ const Game = () => {
   const [timer, setTimer] = useState(null);
   const [score, setScore] = useState(0);
 
+
+  // This shuffle function as found on stack overflow "how to randomize shuffle a javascript array"
   function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
+    console.log(array)
+    setButtonsList(array)
+    // shuffledArray = array
+    console.log(buttonsList)
+
 }
 
-  
-  // const button1Display = () => {
-  //   setButton1(buttonsList[Math.floor(Math.random() * buttonsList.length)]);
-  //   if (button1 !== button2 && button1 !== button3) {
-  //    } else {
-  //      setButton1(buttonsList[Math.floor(Math.random() * buttonsList.length)]);
-  //    }
-  //    return button1;
-  // }
-  // const button2Display = () => {
-  //   setButton2(buttonsList[Math.floor(Math.random() * buttonsList.length)]);
-  //   if (button2 !== button1 && button2 !== button3) {
-  //    return button2;
-  //   } else {
-  //     setButton2(buttonsList[Math.floor(Math.random() * buttonsList.length)]);
-  //   }
-  //   return button2;
-  // }
-  // const button3Display = () => {
-  //   setButton3(buttonsList[Math.floor(Math.random() * buttonsList.length)]);
-  //   if (button3 !== button1 && button3 !== button2) {
-  //    return button3;
-  //   } else {
-  //     setButton3(buttonsList[Math.floor(Math.random() * buttonsList.length)]);
-  //   }
-  //   return button3;
-  // }
+
 
   const currentButton = () => {
     setCurrentChoice(choiceList[Math.floor(Math.random() * choiceList.length)]);
@@ -64,9 +47,7 @@ const Game = () => {
       console.log("hi")
       setCurrentChoice("Game Over");
     }, 1500))
-      // button1Display();
-      // button2Display();
-      // button3Display();
+       shuffleArray(buttonsList)
     if (text === currentChoice) {
       currentButton();
       setScore(() => score + 1);
@@ -103,18 +84,31 @@ const Game = () => {
 
         <div className="game-body-div">
           <div className="click">
-          {/* {button1} */}
-            <p onClick={handleChoice}> Click</p>
+           
+            <p onClick={handleChoice}> {buttonsList[0]} </p>
+          </div>
+
+          <div className="test">
+           
+            <p onClick={handleChoice}>{buttonsList[4]}</p>
+          </div>
+          <div className="test1">
+           
+            <p onClick={handleChoice}>{buttonsList[5]}</p>
+          </div>
+          <div className="test2">
+           
+            <p onClick={handleChoice}>{buttonsList[3]}</p>
           </div>
 
           <div className="turn">
-          {/* {button2} */}
-            <p onClick={handleChoice}>Turn</p>
+           
+            <p onClick={handleChoice}>{buttonsList[1]}</p>
           </div>
 
           <div className="slide">
-          {/* {button3} */}
-            <p onClick={handleChoice}>Slide</p>
+           
+            <p onClick={handleChoice}>{buttonsList[2]} </p>
           </div>
 
           <div id="control-panel">
@@ -126,6 +120,8 @@ const Game = () => {
               Reset
             </button>
           </div>
+          {/* <iframe src="https://www.youtube.com/embed/NSW5u1RTxEA"></iframe> */}
+          
         </div>
       </div>
     </div>
