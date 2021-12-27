6f7d3a7b4f4b46dbfa9e612d3143d00aeba31324
Elle Thompson <elletee@Elles-iMac.attlocal.net>
2021-12-26 18:18:47 -0800

    imported react-alert

--- src/components/Journal.js
+++ src/components/Journal.js
@@ -1,12 +1,13 @@
 import Header from "./Header";
 // import { useState } from "react";
 import { useEffect } from "react";
+import { Alert } from 'react-alert'
 
 const Journal = () => {
   // const [journal, setJournal] = useState();
 
 useEffect (() => {
-  alert("This component will be functional in V2. For now you can enjoy the game :)")
+  Alert("This component will be functional in V2. For now you can enjoy the game :)")
 })
 
 
